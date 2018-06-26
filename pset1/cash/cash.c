0a855b3655112619327cb358326fcb0e9c00a3d5
Jason Wu <laughabc@gmail.com>
2018-06-26 00:56:51 +0000

    comments

--- pset1/cash/cash.c
+++ pset1/cash/cash.c
@@ -6,13 +6,13 @@ int n;
 int main(void) {
 
     do{
-        n = round(get_float("Please enter amount of change: ")*100);
+        n = round(get_float("Please enter amount of change: ")*100);//get a float from user input, multiply by 100,  then round to integer
 
-        int quarter = (n-n%25)/25;
-        int dime = (n%25-n%25%10)/10;
-        int nickle = (n%25%10 - n%25%10%5)/5;
-        int cents = (n%25%10%5);
-        int total = quarter+dime+nickle+cents;
+        int quarter = (n-n%25)/25; //calculate the amount of quarter needed
+        int dime = (n%25-n%25%10)/10; //calculate the amount of dime needed
+        int nickle = (n%25%10 - n%25%10%5)/5; //...nickle needed
+        int cents = (n%25%10%5); //...cents
+        int total = quarter+dime+nickle+cents;//calculate total
 
         printf("%i\n",total);
     }
