8cbf924660c85585096d8d06a638a96edeb48fa9
Jason Wu <lujason2015@gmail.com>
2018-07-09 20:42:16 -0400

    vigenere complete

--- pset2/vigenere/vigenere.c
+++ pset2/vigenere/vigenere.c
@@ -4,52 +4,64 @@
 #include <ctype.h>
 
 //main function with arguement parameter k as string
-
 int main (int argc, string argv[]){
 
 //if number of argument is not one, print error and return 1
-
 if (argc != 2){
     printf("Please run program with one argument\n");
     return 1;
 }
 
+//check if any non alphabets in argument
+for (int i=0; i<strlen(argv[1]); i++){
+    if (!isalpha(argv[1][i])){
+        printf("Argument cannot contrain characters outside of the alphabet\n");
+        return 1;
+    }
+}
+
 //get a word from user as string
 string word = get_string("plaintext: ");
 
-//convert argnument k to an array of numbers by looping through each character(to be refactored)
+//convert argnument k to an array of numbers by looping through each character
 int length = strlen(argv[1]);
-int k = tolower(argv[1][0]);
-k = k - 97;
-printf("First character is: %i\n", k);
-printf("length of arguement: %i\n", length);
-
 int alphaCounter = 0;
-int index = 0;
+int index;
+
+//Begin printing ciphertext
+printf("ciphertext: ");
+
 //loop through characters in the word
 for (int i=0; i<strlen(word); i++){
 
-    printf("%c\n",word[i]);
-///if it is in alphabet, index a number from the argument array of numbers
+///if it is in alphabet, index a number from the argument string
     if(isalpha(word[i])){
 
-        int kj = argv[1][alphaCounter%length];
-        printf ("%i\n", kj);
+        int kj = tolower(argv[1][alphaCounter%length]) - 97; //cycle kj between 0 and length of the argument
+
 ////if capital rotate based on capitalized index, then print
+        if(isupper(word[i])){
+            index = (word[i]-65+kj)%26+65;
 
 ////if not capital rotate based on not capitalized index, then print character
+        }else if(islower(word[i])){
 
-////index to the next number in the argument array using modulo
+            index = (word[i]-97+kj)%26+97;
+        }
+
+////print character then increment to the next alphabetical character in the argument string
+    char c = index;
+    printf("%c", c);
     alphaCounter ++;
 
 
-///else print unchaged character
+///if not an alphabetical character, print it unchaged
     }else{
         printf("%c",word[i]);
     }
 }
 
-//return 0
+//return 0 for success
 printf("\n");
 return 0;
 
