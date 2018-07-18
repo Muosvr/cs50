d06f5acff95014b86bae74c09182878114ea34b3
Jason Wu <lujason2015@gmail.com>
2018-07-17 20:25:39 -0400

    complete

--- pset3/music/helpers.c
+++ pset3/music/helpers.c
@@ -8,21 +8,17 @@
 
 #include "helpers.h"
 
-int main(void){
-    // if (argc != 2){
-    //     printf("Please enter ")
-    //     return 1;
-    // }
+// int main(void){
 
-    string s = get_string("Input duration: ");
-    printf("%i\n",duration(s));
-    // duration(s);
-    string n = get_string("Input note: ");
-    printf("Empty note: %s\n", is_rest(n) ? "true": "false");
-    printf("%i\n",frequency(n));
-    return 0;
+//     string s = get_string("Input duration: ");
+//     printf("%i\n",duration(s));
+//     duration(s);
+//     string n = get_string("Input note: ");
+//     printf("Empty note: %s\n", is_rest(n) ? "true": "false");
+//     printf("%i\n",frequency(n));
+//     return 0;
+// }
 
-}
 
 // Converts a fraction formatted as X/Y to eighths
 int duration(string fraction)
@@ -62,13 +58,13 @@ int frequency(string note)
     //Position correction: if first character is greater than B and less than F, subtract position index by 1
     if(note[0] > 'B' && note[0] < 'F'){
         n ++;
-        printf("Note is between C, D, or E, adjust n by 1\n");
+        // printf("Note is between C, D, or E, adjust n by 1\n");
     }
 
 
 
-    eprintf("position: %i\n", position);
-    eprintf("n: %i\n", n);
+    // eprintf("position: %i\n", position);
+    // eprintf("n: %i\n", n);
 
 
     //check if second character is in alphabet
@@ -84,7 +80,7 @@ int frequency(string note)
             n = n - 1;
         }
 
-        eprintf("# or b: %c\n", note[1]);
+        // eprintf("# or b: %c\n", note[1]);
 
         //convert thrid character: oct = third character - 4
             oct = note[2] - 48;
@@ -100,11 +96,11 @@ int frequency(string note)
 
     // calculate new n based on octave of the note
     n = n+oct*12;
-    eprintf("oct: %i\n",oct);
-    eprintf("n: %i\n", n);
+    // eprintf("oct: %i\n",oct);
+    // eprintf("n: %i\n", n);
 
     //frenquency is 2^(n/12)*440
-    return pow(2,(n/(float)12))*440;
+    return round(pow(2,(n/(float)12))*440);
 }
 
 // Determines whether a string represents a rest
@@ -118,3 +114,4 @@ bool is_rest(string s)
         return false;
     }
 }
+
