334eac20fbfce3bc417b684908c436907feee269
Jason Wu <lujason2015@gmail.com>
2018-07-21 15:01:52 -0400

    whodunit

---
+++ pset4/whodunit/whodunit_pseudo.c
@@ -0,0 +1,23 @@
+//inclue stdio
+
+//main with 2 arguments, input file name and output file name
+
+//check if there are 2 input arguments, if not remind usage with fprintf (to stderr), return 1
+
+//check if the input file can be open for reading. Show error message and return 2 if fails
+
+//check if output file can be open for writing. Show error message and return 3 if fails
+
+//Check if input file is a 24bit uncompressed BMP 4.0. Show error msg and return 4 if false
+
+//copy/update header's info for outfile
+
+//read clue's scanline, pixel by pixel
+
+//change pixel's color as necessary
+
+//write verdict's scanline pixel by pixel
+
+//padding?
+
+//Main return 0 upon success
