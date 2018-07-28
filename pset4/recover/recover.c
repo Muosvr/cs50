003cb27951f7ca00773415231c9aa9f278047d73
Jason Wu <lujason2015@gmail.com>
2018-07-27 21:14:38 -0400

    pseudocode

---
+++ pset4/recover/recover.c
@@ -0,0 +1,19 @@
+//open card file
+
+//repeat until end of card
+
+//read 512 bytes into buffer
+
+//start of a new JPEG?
+
+//yes
+
+//no
+
+//already found a JPEG?
+
+//no
+
+//yes
+
+//close any remaining files
