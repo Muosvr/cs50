da70342d81763ed04737d2dc51f88d025dc19d85
Jason Wu <lujason2015@gmail.com>
2018-07-28 20:56:05 -0400

    recover complete

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -8,7 +8,7 @@ int main(int argc, char *argv[]){
     //check number of input arguments
     if (argc != 2){
         printf("Usage: recover filename\n");
-        return 0;
+        return 1;
     }
 
     printf("File name: %s\n", argv[1]);
@@ -18,7 +18,7 @@ int main(int argc, char *argv[]){
     FILE *input_file = fopen(inputFileName, "r");
 
     //make sure input file is not null
-    if (input_file == NULL ){
+    if (input_file == NULL){
         fprintf(stderr, "Could not open %s.\n", inputFileName);
         return 1;
     }
@@ -26,14 +26,16 @@ int main(int argc, char *argv[]){
     //create buffer for reading
     unsigned char buffer[512]; // use unsigned char to map to each byte
 
-    //declare counting variables
+    //declare variables
     int counter = 0;
-    int jpegCounter = 0;
+    int jpegCounter = -1;//so the image file namges starts with 0
     int foundJPEG = 0;
+    int a=0, b=0, c=0;
+    char outputNameBuffer[3];
 
     //test
-    char *outputFileName = "test.jpeg";
-    FILE *output_file = fopen(outputFileName, "w");
+    sprintf(outputNameBuffer,"%d%d%d.jpg",c,b,a);
+    FILE *output_file = fopen(outputNameBuffer, "w");
 
     //repeat until end of card
     //read 512 bytes into buffer
@@ -47,25 +49,24 @@ int main(int argc, char *argv[]){
             break;
         }
 
-        if (jpegCounter > 1){
-            break;
-        }
-
-        // printf("%s\n", buffer[1]);
         //start of a new JPEG?
         if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff
         && (buffer[3] & 0xf0) == 0xe0){
             jpegCounter ++;
 
             //Alredy found a JPEG?
+            if(foundJPEG == 1){
+                fclose(output_file);
 
-                //Yes -> close existing JPEG file
+                //specify naming convention
+                a = jpegCounter%10;
+                b = jpegCounter%100/10;
+                c = jpegCounter%1000/100;
+                sprintf(outputNameBuffer,"%d%d%d.jpg",c,b,a);
 
-            //create new output file name
-            // char *outputFileName = "test.jpeg";
-
-            //open output file
-            // FILE *output_file = fopen(outputFileName, "w");
+                //create file under new name
+                output_file = fopen(outputNameBuffer, "w");
+            }
 
             //write into new output file
             fwrite(buffer,512,1,output_file);
@@ -73,23 +74,23 @@ int main(int argc, char *argv[]){
             //set found JPEG to be true
             foundJPEG = 1;
 
+        //if not the start of a JPEG
+        }else{
 
-    }else{
-        //already found a JPEG?
-        if (foundJPEG == 1){
-            //Yes -> write buffer into existing output file
-            fwrite(buffer,512,1,output_file);
+            //already found a JPEG?
+            if (foundJPEG == 1){
+
+                //Yes -> write buffer into existing output file
+                fwrite(buffer,512,1,output_file);
+            }
         }
-
-
-
-    }
-
     }
 
 //close any remaining files
 fclose(output_file);
 
+//output to confirm reading of blocks and counted images
 printf("%i blocks read\n", counter);
-printf("This card has %i jpg pictures\n", jpegCounter);
+printf("This card has %i jpg pictures\n", jpegCounter+1);
+return 0;
 }
