d134b938728e3dc2d5b7cff5942a62633e1e1776
Jason Wu <lujason2015@gmail.com>
2018-08-07 21:17:19 -0400

    external hash function experiment

---
+++ pset5/speller/dictionaries/hash.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <ctype.h>
+
+#define HASHTABLE 100
+#define MAXSTR 46
+
+// djb2 hash function from http://www.cse.yorku.ca/~oz/hash.html
+//experimenting codes from https://www.reddit.com/r/C_Programming/comments/2syijd/understanding_this_hash_function/
+unsigned long hash_func (char* word)
+{
+    unsigned long hash = 5381;
+    int c;
+
+    while ((c = *word++))
+    {
+        printf("int c = %i\n", c); // only to visualize function
+
+        hash = ((hash << 5) + hash) + tolower(c);
+        printf("hash = %lu\n\n", hash); // only to visualize function
+    }
+    return hash % HASHTABLE;
+}
+
+int main(void)
+{
+    char word[MAXSTR];
+
+    printf("Word to hash- ");
+    scanf("%s45", word);
+
+    int index = hash_func(word);
+    printf("index = %i\n", index);
+}
